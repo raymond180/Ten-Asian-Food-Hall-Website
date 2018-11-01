db80878dbc4cf43710f43adf92c23a7eeb989bc3
raymond180 <raymond180@hotmail.com>
2018-11-01 18:02:34 -0400

    fixed carousel

---
+++ carousel.php
@@ -0,0 +1,47 @@
+            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#myCarousel" data-slide-to="1"></li>
+                    <li data-target="#myCarousel" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <div class="text-center">
+                            <img class="img-fluid" src="./images/five-cups.jpg" alt="Drinks">
+                        </div>
+                        <div class="container">
+                            <div class="carousel-caption text-left">
+                                <p><a class="btn btn-md btn-dark" href="./customer-order.php" role="button">Order online</a></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="text-center">
+                            <img class="img-fluid" src="./images/fried-chicken-and-buble.jpg" alt="Food">
+                        </div>
+                        <div class="container">
+                            <div class="carousel-caption text-left">
+                                <p><a class="btn btn-md btn-dark " href="./menu.php" role="button">Browse menu</a></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="text-center">
+                            <img class="img-fluid" src="./images/six-cups.jpg" alt="Store">
+                        </div>
+                        <div class="container">
+                            <div class="carousel-caption text-left">
+                                <p><a class="btn btn-md btn-dark" href="./index.php#story" role="button">About us</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
--- customer-order.php
+++ customer-order.php
@@ -16,55 +16,7 @@
 		
 		<!-- Main Content Start -->
         <main role="main">
-        <!-- Carousel Start -->
-            <div id="myCarousel" class="carousel slide" data-ride="carousel">
-                <ol class="carousel-indicators">
-                    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                    <li data-target="#myCarousel" data-slide-to="1"></li>
-                    <li data-target="#myCarousel" data-slide-to="2"></li>
-                </ol>
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <div class="text-center">
-                            <img class="img-fluid" src="./images/five-cups.jpg" alt="Drinks">
-                        </div>
-                        <div class="container">
-                            <div class="carousel-caption text-left">
-                                <p><a class="btn btn-md btn-dark" href="./customer-order.html" role="button">Order online</a></p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                        <div class="text-center">
-                            <img class="img-fluid" src="./images/fried-chicken-and-buble.jpg" alt="Food">
-                        </div>
-                        <div class="container">
-                            <div class="carousel-caption text-left">
-                                <p><a class="btn btn-md btn-dark " href="./menu.html" role="button">Browse menu</a></p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                        <div class="text-center">
-                            <img class="img-fluid" src="./images/six-cups.jpg" alt="Store">
-                        </div>
-                        <div class="container">
-                            <div class="carousel-caption text-left">
-                                <p><a class="btn btn-md btn-dark" href="./index.html#story" role="button">About us</a></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
-            </div>
-        <!-- Carousel End -->
+        <!-- Carousel Start --><?php require_once'carousel.php'; ?><!-- Carousel End -->
 		
 		<!-- Content Start -->
 		<section>
