26dcb44b67c0a001754b4a6a4c1cd525f8ce004c
raymond180 <raymond180@hotmail.com>
2018-11-03 17:51:50 -0400

    management top navbar

--- management/inventory.php
+++ management/inventory.php
@@ -14,15 +14,7 @@
     <title>Ten Asian Management</title>
 </head>
 <body>
-    <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-        <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Ten Asian Management</a>
-        <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
-        <ul class="navbar-nav px-3">
-            <li class="nav-item text-nowrap">
-                <a class="nav-link" href="#">Sign out</a>
-            </li>
-        </ul>
-    </nav>
+    <!-- Top NavBar Start--><?php require_once'navbar-top.php'; ?><!-- Top NavBar End -->
     <div class="container-fluid">
         <div class="row">
             <nav class="col-md-2 d-none d-md-block bg-light sidebar">
