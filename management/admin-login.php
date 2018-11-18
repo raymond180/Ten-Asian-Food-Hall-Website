5243dc3b6e94b5a49165ef471f244745a3de2367
jiayuwang151 <jwang151@terpmail.umd.edu>
2018-11-18 16:01:04 -0500

    admin login php

--- management/admin-login.php
+++ management/admin-login.php
@@ -1,3 +1,43 @@
+<?php
+  // NOTE: YOU MAY LOGIN WITH username "user1" or "user2" and password "password"
+
+  require_once('../dbhelper/dbhelper.php');
+  session_start();
+
+  //if already logged in, redirect to homepage
+  if(isset($_SESSION['employeeLogin'])) {
+    header('Location: dashboard.php');
+  }
+
+   // check if the form was submitted by checking to see if PHP automatically set the $_POST variable
+   // if the form was submitted, we want to run the code to verify their login.
+   // if not, none of the code inside this block runs --> we skip it and don't do anything
+  if (isset($_POST['submit'])) {
+
+    $employeeLoginFromForm = $_POST['employeeLogin'];
+    $employeePasswordFromForm = $_POST['employeePassword'];
+
+    // construct your query--> note that this does not actually do anything! We need to run the query below.
+    $query = "SELECT employeeLogin , employeePassword FROM `Employees` WHERE `employeeLogin` = '{$employeeLoginFromForm}'";
+
+    // runs the query and returns the one record. We know there should only be one person with a given username
+    $record = getOneRow($query);
+
+    // Checks if the username & password from the form matches what is in the DB
+    if ($record['employeeLogin'] == $employeeLoginFromForm AND $record['employeePassword'] == $employeePasswordFromForm) {
+      // they are authenticated --> store the session variable to log them in
+      $_SESSION['employeeLogin'] = $employeeLoginFromForm;
+
+      // redirect them to the homepage
+      header('Location: dashboard.php');
+    } else {
+      // display invalid error message
+      $errorMessage = "Invalid username or password.";
+    }
+
+  }
+?>
+
 <!doctype html>
 <html lang="en">
     <head>
@@ -14,23 +54,23 @@
 
         <!-- Body starts here -->
     <body class="text-center">
-        <form class="form-signin">
+        <form action="admin-login.php" method="POST"  class="form-signin" >
           <img class="img-fluid" src="./images/admin-login.jpg">
           <h1 class="h3 mb-3 font-weight-normal">Employee Sign in</h1>
            <!-- Put into the account/Employee Number here -->
           <label for="inputEmployeeNumber" class="sr-only">Employee Number</label>
-          <input type="email" id="inputEmployeeNumber" class="form-control" placeholder="Employee Number required" required autofocus>
+          <input type="text" name="employeeLogin" id="inputEmployeeNumber" class="form-control" placeholder="Employee Number required" required autofocus>
              <!-- Put into the Password here -->
           <label for="inputPassword" class="sr-only">Password</label>
-          <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+          <input type="password" name="employeePassword" id="inputPassword"  class="form-control" placeholder="Password" required>
           <div class="checkbox mb-3">
             <label>
                <!-- Make the website remember the account and password -->
-              <input type="checkbox" value="remember-me"> Remember me
+              <input type="checkbox" value="remember-me" > Remember me
             </label>
           </div>
          
-          <button class="btn btn-lg btn-dark btn-block" type="submit">Sign in</button>
+          <button class="btn btn-lg btn-dark btn-block" type="submit" name="submit">Sign in</button>
           <p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
         </form> 
        
